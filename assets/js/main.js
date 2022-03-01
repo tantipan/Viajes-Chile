99dfe04a58f446d176017e110e8fd1e1ba931bb0
tantipan <tantipannunez@gmail.com>
2022-03-01 11:47:17 -0300

    smooth scroll listo, tooltip listo, toggle listo (3 animaciones).

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
+
+$('.card-title').click(function() {
+
+  $(".card-text").toggle();
+});
+
+
+
 $(document).ready(function(){
-  // Add smooth scrolling to nav-link
+  // Add smooth scrolling to all links
   $(".nav-link").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
@@ -12,9 +20,21 @@ $(document).ready(function(){
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate({
+      $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800);
+      }, 1500, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
     } // End if
   });
+});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(function () {
+  $('[data-toggle="popover"]').popover();
 });
