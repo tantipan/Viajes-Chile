c14b9671950ac954594d9eeec545848b8b1815c1
tantipan <tantipannunez@gmail.com>
2022-03-02 01:13:27 -0300

    Entrega

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ $('.card-title').click(function() {
 
 $(document).ready(function(){
   // Add smooth scrolling to all links
-  $(".nav-link").on('click', function(event) {
+  $(".navbar.nav-item.nav-link").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
